4105ecf78d54cfb2ad465269d28c47d1b922bd72
OreoluwaElizabeth <oreoluwaadeyemo77@gmail.com>
2024-04-14 20:53:01 -0800

    added files

---
+++ test_logistics.py
@@ -0,0 +1,11 @@
+import logistics
+
+payment1 = logistics.calculate_payment(25)
+payment2 = logistics.calculate_payment(55)
+payment3 = logistics.calculate_payment(65)
+payment4 = logistics.calculate_payment(80)
+
+print("Payment for 25 deliveries:", payment1)
+print("Payment for 55 deliveries:", payment2)
+print("Payment for 65 deliveries:", payment3)
+print("Payment for 80 deliveries:", payment4)
