ac4479ab4dd8257150d452de7efbcf6afdbc2853
OreoluwaElizabeth <oreoluwaadeyemo77@gmail.com>
2024-04-27 04:10:21 -0800

    New files added

--- circle.py
+++ circle.py
@@ -1,33 +1,11 @@
-<<<<<<< HEAD
-pie = 3.14159
+import math
 
-radius = 2
+PI = math.pi
 
-diameter = 2 * radius
+def area(radius):
 
-circumference = 2 * pie * radius
+	area_of_circle = PI * (radius * radius)
+	
+	return f"The area of the circle is: {area_of_circle:.2f}"
 
-area = (pie * radius) ** (pie * radius)
-
-print(f" The diameter of the circle is: {diameter}")
-
-print(f" The circumference of the circle is: {circumference:.2f}")
-
-print(f" The area of the circle is: {area:.2f}")
-=======
-pie = 3.14159
-
-radius = 2
-
-diameter = 2 * radius
-
-circumference = 2 * pie * radius
-
-area = (pie * radius) ** (pie * radius)
-
-print(f" The diameter of the circle is: {diameter}")
-
-print(f" The circumference of the circle is: {circumference:.2f}")
-
-print(f" The area of the circle is: {area:.2f}")
->>>>>>> 6a3fab508672973e079e375c249b62231f60b629
+print(area(4))
